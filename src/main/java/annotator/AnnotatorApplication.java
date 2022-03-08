68d8d824a2918e47f23024dd78eab444168702c5
sarra-benmohamed-titanom <91532221+sarra-benmohamed-titanom@users.noreply.github.com>
2022-03-08 15:45:00 +0100

    feat: initial commit

---
+++ src/main/java/annotator/AnnotatorApplication.java
@@ -0,0 +1,13 @@
+package annotator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/annotator/AnnotatorApplicationTests.java
@@ -0,0 +1,13 @@
+package annotator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
